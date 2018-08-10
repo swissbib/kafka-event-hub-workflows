d31fccc9782a01ce2f0cfee1a1fd96805d9ae807
Kordishal <jonaswaeber@gmail.com>
2018-08-10 16:03:20 +0200

    Fixed BOM-bug again: On server only utf-16 works...

--- kafkaflows/digi/dsv01/dsv01_sru_to_kafka.py
+++ kafkaflows/digi/dsv01/dsv01_sru_to_kafka.py
@@ -1,7 +1,7 @@
 from kafka_event_hub.producers import SRUProducer
 
 if __name__ == '__main__':
-    with open('data/dsv01_system_numbers_vor_1900_arc_export_20180802.csv', 'r', encoding='utf-8-sig') as file:
+    with open('data/dsv01_system_numbers_vor_1900_arc_export_20180802.csv', 'r', encoding='utf-16') as file:
         producer = SRUProducer('config/dsv01_dump.yml')
         for line in file:
             year, sys_number = line.split(',')
