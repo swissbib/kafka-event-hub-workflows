40f0ce306f970fc62f6d68dae51f2df09f1921ad
Kordishal <jonaswaeber@gmail.com>
2018-08-13 13:46:33 +0200

    Added 'u' to 008 date type codes.

--- kafkaflows/digi/utility/mapper.py
+++ kafkaflows/digi/utility/mapper.py
@@ -82,7 +82,7 @@ class MARCMapper(object):
             self.logger.info('No date defined in field 008 (code n)')
             return False
 
-        if _008[6] not in ['s', 'm', 'q', 'i', 'd', 'r']:
+        if _008[6] not in ['s', 'm', 'q', 'i', 'd', 'r', 'u']:
             self.logger.error('Unexpected value in field 008 F6 (date type): %s.', _008[6])
             return False
 
@@ -108,7 +108,7 @@ class MARCMapper(object):
                 self.result['dates']['date']['original'] = year_2
             return True
 
-        if _008[6] in ['m', 'q', 'i', 'd']:
+        if _008[6] in ['m', 'q', 'i', 'd', 'u']:
             self.result['dates']['date']['year'] = year_1
             if isinstance(year_2, int):
                 self.result['dates']['date']['to'] = year_2
