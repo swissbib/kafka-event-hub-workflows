efc34973b88c6a0a2bd76116878ceed1910fcac8
Kordishal <jonaswaeber@gmail.com>
2018-08-13 11:03:52 +0200

    Added logging to error messages in the run module. Fixed consumer configs.

--- run/run.py
+++ run/run.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import logging
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kafkaflows.digi.dsv01 import run_dsv01_producer
 from kafkaflows.digi.dsv01 import run_dsv01_consumer
@@ -12,11 +13,21 @@ parser.add_argument('script', action='store')
 
 args = parser.parse_args()
 
-if args.script == 'dsv05-consumer':
-    run_dsv05_consumer()
-elif args.script == 'dsv05-producer':
-    run_dsv05_producer()
-elif args.script == 'dsv01-producer':
-    run_dsv01_producer()
-elif args.script == 'dsv01-consumer':
-    run_dsv01_consumer()
+logging.basicConfig(filename='logs/{}.log'.format(args.script), filemode='w', level=logging.INFO,
+                    format='%(levelname)s|%(name)s|%(created)f|%(message)s')
+
+try:
+    if args.script == 'dsv05-consumer':
+        run_dsv05_consumer()
+    elif args.script == 'dsv05-producer':
+        run_dsv05_producer()
+    elif args.script == 'dsv01-producer':
+        run_dsv01_producer()
+    elif args.script == 'dsv01-consumer':
+        run_dsv01_consumer()
+except Exception as e:
+    logging.exception(e)
+    sys.exit(1)
+else:
+    logging.info('Application ended without error!')
+
