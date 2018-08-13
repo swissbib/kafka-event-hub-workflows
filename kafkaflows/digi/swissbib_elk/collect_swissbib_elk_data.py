3ead85fd81cb9092b727d5a392760892c74d0b18
Kordishal <jonaswaeber@gmail.com>
2018-08-13 16:13:42 +0200

    Fixed swissbib-elk script.

--- kafkaflows/digi/swissbib_elk/collect_swissbib_elk_data.py
+++ kafkaflows/digi/swissbib_elk/collect_swissbib_elk_data.py
@@ -6,28 +6,29 @@ def run_swissbib_elk():
     kafka_dsv05 = ElasticIndex('kafka-dsv05', doc_type='record', url='http://sb-ues2.swissbib.unibas.ch:9200')
 
     for instance in kafka_dsv01, kafka_dsv05:
-        for record in instance.scroll():
-            identifier = record['identifier']
+        for results in instance.scroll():
+            for record in results:
+                identifier = record['identifier']
 
-            hits = dict()
-            for year in range(2017, 2019):
-                sru = ElasticIndex('sru-{}'.format(year), doc_type='logs', url='http://sb-uesl1.swissbib.unibas.ch:9200')
-                hits['sru'] = dict()
-                query = {'query': {'match': {'requestparams': identifier}}}
-                hits['sru'][str(year)] = len(sru.scan_index(query=query))
-
-            for source in ['green', 'jus', 'bb']:
-                hits[source] = dict()
+                hits = dict()
                 for year in range(2017, 2019):
-                    swissbib = ElasticIndex('swissbib-{}-{}'.format(source, year),
-                                            doc_type='logs',
-                                            url='http://sb-uesl1.swissbib.unibas.ch:9200')
+                    sru = ElasticIndex('sru-{}'.format(year), doc_type='logs', url='http://sb-uesl1.swissbib.unibas.ch:9200')
+                    hits['sru'] = dict()
+                    query = {'query': {'match': {'requestparams': identifier}}}
+                    hits['sru'][str(year)] = len(sru.scan_index(query=query))
 
-                    query = {'query': {'term': {'request_middle.keyword': {'value': identifier}}}}
-                    hits[source][str(year)] = len(swissbib.scan_index(query=query))
+                for source in ['green', 'jus', 'bb']:
+                    hits[source] = dict()
+                    for year in range(2017, 2019):
+                        swissbib = ElasticIndex('swissbib-{}-{}'.format(source, year),
+                                                doc_type='logs',
+                                                url='http://sb-uesl1.swissbib.unibas.ch:9200')
 
-            record['hits'] = hits
-            instance.index_into(record, identifier)
+                        query = {'query': {'term': {'request_middle.keyword': {'value': identifier}}}}
+                        hits[source][str(year)] = len(swissbib.scan_index(query=query))
+
+                record['hits'] = hits
+                instance.index_into(record, identifier)
 
 
 
