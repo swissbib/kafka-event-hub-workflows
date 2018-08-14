8248664c3b11aa5195bc511da0f83ece0033887a
Kordishal <jonaswaeber@gmail.com>
2018-08-14 13:14:23 +0200

    fix(digispace): Fixed tunnel command.

--- run/job.start.digispace.producer.sh
+++ run/job.start.digispace.producer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-ssh -f -o ExitOnForwardFailure=yes -L 9200:ub-afrikaportal.ub.unibas.ch:9200 sleep 10
+ssh -f -o ExitOnForwardFailure=yes -L 9200:localhost:9200 harvester@ub-afrikaportal.ub.unibas.ch:9200
+
+sleep 10
 
 setsid python run.py digispace > logs/stdout.log 2>&1 < /dev/null &
