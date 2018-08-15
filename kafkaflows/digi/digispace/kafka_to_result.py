f3a107a2466b98d609b11a7c7742a87cf2ec074a
Kordishal <jonaswaeber@gmail.com>
2018-08-15 10:51:00 +0200

    fix(digispace): handle different key value.
    The field sys_id in digidata can contain special keys of the form:
    1_BAU_1_\d{9}. These are simply ignored for now.

--- kafkaflows/digi/digispace/kafka_to_result.py
+++ kafkaflows/digi/digispace/kafka_to_result.py
@@ -19,33 +19,36 @@ def run_digispace_kafka_to_result(config):
     consumer = SimpleConsumer(config['consumer.path'])
     while True:
         for key, value in consumer.consume(num_messages=100):
-            db, sys_number = key.split('_')
-
-            query = {
-                'query': {
-                    'term': {
-                        'identifiers.{}'.format(db_translation[db]): {
-                            'value': sys_number
+            try:
+                db, sys_number = key.split('_')
+            except ValueError:
+                pass
+            else:
+                query = {
+                    'query': {
+                        'term': {
+                            'identifiers.{}'.format(db_translation[db]): {
+                                'value': sys_number
+                            }
                         }
                     }
                 }
-            }
-            results = index.scan_index(query=query)
-            if len(results) == 1:
-                digidata = json.loads(value)
-                record = results[0]
-                record['digidata'] = dict()
-                record['digidata']['is_digitised'] = True
-                if 'images' in digidata:
-                    record['digidata']['images'] = digidata['images']
-                if db == '1':
-                    dsv01.index_into(record, record['identifier'])
+                results = index.scan_index(query=query)
+                if len(results) == 1:
+                    digidata = json.loads(value)
+                    record = results[0]
+                    record['digidata'] = dict()
+                    record['digidata']['is_digitised'] = True
+                    if 'images' in digidata:
+                        record['digidata']['images'] = digidata['images']
+                    if db == '1':
+                        dsv01.index_into(record, record['identifier'])
+                    else:
+                        dsv05.index_into(record, record['identifier'])
+                elif len(results) == 0:
+                    logging.error('Found no records for %s form %s.', sys_number, db_translation[db])
                 else:
-                    dsv05.index_into(record, record['identifier'])
-            elif len(results) == 0:
-                logging.error('Found no records for %s form %s.', sys_number, db_translation[db])
-            else:
-                logging.error('Found multiple results for %s from %s.', sys_number, db_translation[db])
+                    logging.error('Found multiple results for %s from %s.', sys_number, db_translation[db])
 
 
 
