c514c65a658e6a22828b72b9a09a211c0177a035
Kordishal <jonaswaeber@gmail.com>
2018-08-17 10:14:45 +0200

    feat(dsv01): add producers for sru queries. . Add producer to query IDSBB*, A100, before 1920. Add producer to query IDSBB*, A125, before 1920.

--- kafkaflows/digi/dsv01/__init__.py
+++ kafkaflows/digi/dsv01/__init__.py
@@ -1,2 +1,3 @@
 from kafkaflows.digi.dsv01.dsv01_kafka_to_elastic import run_dsv01_consumer
-from kafkaflows.digi.dsv01.dsv01_sru_to_kafka import run_dsv01_producer
+from kafkaflows.digi.dsv01.dsv01_sru_to_kafka import run_dsv01_producer, run_general_dsv01_a100_producer, \
+    run_general_dsv01_a125_producer
