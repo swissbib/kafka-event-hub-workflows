7096f132e1a3a986328b9d6ae4ae34ce8c766498
Kordishal <jonaswaeber@gmail.com>
2018-08-31 16:36:19 +0200

    chore(ignore): ignore idea folder.

--- kafkaflows/digi/dsv05/dsv05_sru_to_kafka.py
+++ kafkaflows/digi/dsv05/dsv05_sru_to_kafka.py
@@ -1,7 +1,19 @@
 from kafka_event_hub.producers import SRUProducer
+import json
 
 
 def run_dsv05_producer(config):
     p = SRUProducer(config['producer.path'])
     p.set_query_id_equal_with('HAN*')
     p.process()
+
+
+def run_dsv05_producer_pre_compiled_list(config):
+    with open('data/dsv05_system_numbers.json', 'r') as file:
+        producer = SRUProducer(config['producer.path'])
+        sys_numbers = json.load(file)
+        for sys_number in sys_numbers:
+            while len(sys_number) < 10:
+                sys_number = '0' + sys_number
+            producer.set_query_id_equal_with('HAN' + sys_number)
+            producer.process()
