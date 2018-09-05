91813d50e3fa3b59e3ffdfae359cb1d191f2e4e2
Kordishal <jonaswaeber@gmail.com>
2018-09-05 12:10:30 +0200

    fix(enrich): add config for indexes to be enriched.

--- kafkaflows/digi/user_data/enrichment.py
+++ kafkaflows/digi/user_data/enrichment.py
@@ -3,25 +3,27 @@ from kafkaflows.digi.user_data import swissbib, aleph, e_plattforms, opac
 
 
 def enrich_user_data(config):
-    index = ElasticIndex('kafka-dsv01-v7', 'record', url='http://sb-ues2.swissbib.unibas.ch:9200')
 
-    for results in index.scroll():
-        for item in results:
+    for index in config['indexes']:
+        instance = ElasticIndex(**index)
 
-            document = dict()
-            identifier = item['identifier']
-            sys_number = item['identifiers'][item['database']]
+        for results in instance.scroll():
+            for item in results:
 
-            document['hits'] = dict()
-            error_tags_list = list()
-            hits, error_tags = swissbib.enrich(sys_number)
-            document['hits']['swissbib'] = hits
-            error_tags_list.extend(error_tags)
+                document = dict()
+                identifier = item['identifier']
+                sys_number = item['identifiers'][item['database']]
 
-            hits, error_tags = opac.enrich(sys_number)
-            document['hits']['opac-access'] = hits
-            error_tags_list.extend(error_tags)
+                document['hits'] = dict()
+                error_tags_list = list()
+                hits, error_tags = swissbib.enrich(sys_number)
+                document['hits']['swissbib'] = hits
+                error_tags_list.extend(error_tags)
 
-            index.update(document, doc_id=identifier)
-            for tag in error_tags_list:
-                index.script_update('ctx._source.error_tags.add(params.tag)', {'tag': tag}, doc_id=identifier)
+                hits, error_tags = opac.enrich(sys_number)
+                document['hits']['opac-access'] = hits
+                error_tags_list.extend(error_tags)
+
+                instance.update(document, doc_id=identifier)
+                for tag in error_tags_list:
+                    instance.script_update('ctx._source.error_tags.add(params.tag)', {'tag': tag}, doc_id=identifier)
