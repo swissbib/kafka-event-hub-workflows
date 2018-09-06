45c8ec9ca15ccffd0d8c4fb8de093282b320fc9c
Kordishal <jonaswaeber@gmail.com>
2018-09-06 14:01:45 +0200

    fix(erara): e-rara enrich
    return zero counts when no match is found.

--- kafkaflows/digi/user_data/e_rara/__init__.py
+++ kafkaflows/digi/user_data/e_rara/__init__.py
@@ -17,5 +17,9 @@ def enrich(system_number: str) -> Tuple[Dict[str, Dict[str, int]], Union[List[st
                 }
             }
         }
-    hits = index.scan_index(query=query)[0]
-    return hits, []
+
+    results = index.scan_index(query=query)
+    if len(results) == 1:
+        return results[0], []
+    else:
+        return {'bau': {'2016': 0, '2017': 0, '2018': 0, 'total': 0}}, []
