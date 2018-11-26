2948de223e1ace428e672818e716cf420f7f61c0
Jonas Waeber <jonaswaeber@gmail.com>
2018-11-26 13:32:51 +0100

    update export data

--- kafkaflows/digi/scripts/export_data.py
+++ kafkaflows/digi/scripts/export_data.py
@@ -4,12 +4,4 @@ import json
 
 if __name__ == '__main__':
     data = ElasticIndex('e-codices-data', 'hits')
-
-    export = list()
-    for results in data.scroll():
-        export.extend(results)
-        print(len(export))
-    print(len(export))
-
-    with open('elias-data-e-codices-20181022.json', 'w') as fp:
-        json.dump(export, fp)
+    data.dump(".")
